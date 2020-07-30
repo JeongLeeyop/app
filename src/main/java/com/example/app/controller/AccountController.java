41dd13a90a67729b7134688d24c601956b53aa5f
Jeongleeyop <anfdmavy777@naver.com>
2020-07-31 05:03:56 +0900

    Merge branch '07-28Setting' of https://github.com/JeongLeeyop/app into 07-28Setting
     Conflicts:
    	.idea/workspace.xml

--- src/main/java/com/example/app/controller/AccountController.java
+++ src/main/java/com/example/app/controller/AccountController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,6 +34,36 @@ public class AccountController {
     //2.회원가입 버튼
     @RequestMapping("/signUp")
     public String signUp(accountRequest account, HttpServletRequest req) {
+//        try {
+//
+//            String pw = account.getPassword();
+//            System.out.println(pw);
+//
+//            // SHA-256 MessageDigest의 생성
+//            MessageDigest mdSHA256 = MessageDigest.getInstance("sha256");
+//
+//            // " Java 마스터! " 문자열 바이트로 메시지 다이제스트를 갱신
+//            mdSHA256.update(pw.getBytes("UTF-8"));
+//
+//            // 해시 계산 반환값은 바이트 배열
+//            byte[] sha256Hash = mdSHA256.digest();
+//
+//            // 바이트배열을 16진수 문자열로 변환하여 표시
+//            StringBuilder hexSHA256hash = new StringBuilder();
+//            for (byte b : sha256Hash) {
+//                String hexString = String.format("%02x", b);
+//                hexSHA256hash.append(hexString);
+//            }
+//
+//            System.out.println(hexSHA256hash);
+//        }catch(NoSuchAlgorithmException e){
+//
+//            e.printStackTrace();
+//
+//            SHA = null;
+//
+//        }
+
         return "redirect:login";
     }
 
